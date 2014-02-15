9c030022ddae4f908dceb35874df8a9b0bb1a042
Ronan Collobert <ronan@collobert.com>
2014-02-15 13:09:57 +0100

    FindReadline: forget ncurses for now (obsolete?)

--- luajit/cmake/FindReadline.cmake
+++ luajit/cmake/FindReadline.cmake
@@ -5,7 +5,6 @@
 #  READLINE_FOUND, If false, do not try to use READLINE.
 # also defined, but not for general use are
 # READLINE_readline_LIBRARY, where to find the READLINE library.
-# READLINE_ncurses_LIBRARY, where to find the ncurses library [might not be defined]
 
 # Apple readline does not support readline hooks
 # So we look for another one by default
@@ -36,23 +35,9 @@ IF(APPLE)
 ENDIF(APPLE)
 FIND_LIBRARY(READLINE_readline_LIBRARY NAMES readline)
 
-# Sometimes readline really needs ncurses
-IF(APPLE)
-  FIND_LIBRARY(READLINE_ncurses_LIBRARY NAMES ncurses PATHS
-    /sw/lib
-    /opt/local/lib
-    /opt/lib
-    /usr/local/lib
-    /usr/lib
-    NO_DEFAULT_PATH
-    )
-ENDIF(APPLE)
-FIND_LIBRARY(READLINE_ncurses_LIBRARY NAMES ncurses)
-
 MARK_AS_ADVANCED(
   READLINE_INCLUDE_DIR
   READLINE_readline_LIBRARY
-  READLINE_ncurses_LIBRARY
   )
 
 SET( READLINE_FOUND "NO" )
@@ -62,11 +47,6 @@ IF(READLINE_INCLUDE_DIR)
     SET( READLINE_LIBRARIES
       ${READLINE_readline_LIBRARY} 
       )
-
-    # some readline libraries depend on ncurses
-    IF(READLINE_ncurses_LIBRARY)
-      SET(READLINE_LIBRARIES ${READLINE_LIBRARIES} ${READLINE_ncurses_LIBRARY})
-    ENDIF(READLINE_ncurses_LIBRARY)
 
   ENDIF(READLINE_readline_LIBRARY)
 ENDIF(READLINE_INCLUDE_DIR)
