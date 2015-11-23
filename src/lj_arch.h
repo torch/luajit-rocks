09c1591e5cfdbf21fbe350de34fd94b971223c1e
Ronan Collobert <locronan@fb.com>
2015-11-23 13:16:25 -0800

    Squashed 'luajit-2.0/' changes from 8715ae6..367cba2
    367cba2 DynASM/x86: Add rdpmc instruction.
    7991a66 Extend Valgrind suppressions for non-inlined C code.
    1393b2f Update build instructions.
    f416cf6 OSX: Switch to Clang as the default compiler.
    3639ff4 iOS: Disable os.execute() when building for iOS >= 8.0.
    7579b16 DynASM/x86: Restrict shld/shrd to operands with same width.
    9dc785e DynASM/x86: Fix some SSE instruction templates.
    e5f4cb1 FFI: Fix ipairs() recording.
    4808e62 Drop marks from replayed instructions when sinking.
    2e85af8 Flush symbol tables in jit.dump on trace flush.
    ef087aa Fix unsinking check.
    076d625 Fix Cygwin support. Note: this is not a supported target.
    
    git-subtree-dir: luajit-2.0
    git-subtree-split: 367cba29ea03aa758eb041c3a0c5f1c7616f9a0a

--- src/lj_arch.h
+++ src/lj_arch.h
@@ -151,7 +151,11 @@
 #define LJ_ARCH_NAME		"x64"
 #define LJ_ARCH_BITS		64
 #define LJ_ARCH_ENDIAN		LUAJIT_LE
-#define LJ_ABI_WIN		LJ_TARGET_WINDOWS
+#if LJ_TARGET_WINDOWS || __CYGWIN__
+#define LJ_ABI_WIN		1
+#else
+#define LJ_ABI_WIN		0
+#endif
 #define LJ_TARGET_X64		1
 #define LJ_TARGET_X86ORX64	1
 #define LJ_TARGET_EHRETREG	0
@@ -421,6 +425,9 @@
 #endif
 #if defined(__symbian__)
 #define LUAJIT_NO_EXP2
+#endif
+#if LJ_TARGET_CONSOLE || (LJ_TARGET_IOS && __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_8_0)
+#define LJ_NO_SYSTEM		1
 #endif
 
 #if defined(LUAJIT_NO_UNWIND) || defined(__symbian__) || LJ_TARGET_IOS || LJ_TARGET_PS3 || LJ_TARGET_PS4
